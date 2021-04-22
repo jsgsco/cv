98722eb0c69ccf1a0f2bc66703741dd078624e1b
Jhon Steven Saavedra <65989472+jsgsco@users.noreply.github.com>
2021-04-22 17:40:11 -0500

    Fix finish proyect

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,6 @@
 // Import from Home Page
 import Home from '../components/pages/Home/Home'
 
-// Import from Contact Page
-import Contact from '../components/pages/Contact/Contact'
-
-
 // Import from Error Page
 import Error from '../components/pages/Error/Error404'
 
